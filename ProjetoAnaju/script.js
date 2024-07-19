68d2275bbe7f5232f244e61be4d18b7f224ae3a6
Ana Júlia Bettio <ana.bettio@prototipandoaquebrada.org>
2024-07-19 18:46:35 -0300

    Delete ProjetoAnaju directory

--- ProjetoAnaju/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('Formulariodecontato').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Mensagem enviada com sucesso!');
-    // Aqui você vai adicionar código para enviar o formulário via wpp, depois
-});
