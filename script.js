35fe237897d581f4388e0ae1583841eabb81d296
Ana Júlia Bettio <ana.bettio@prototipandoaquebrada.org>
2024-07-19 18:47:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('Formulariodecontato').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+    // Aqui você vai adicionar código para enviar o formulário via wpp, depois
+});
