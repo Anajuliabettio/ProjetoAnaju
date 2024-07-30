1a5c94ac022d103e8e35b6cc9a43d7d1a97e15d9
anajulia <ana.bettio@prototipandoaquebrada.org>
2024-07-30 13:35:33 -0300

    Ajustes finais

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-document.getElementById('Formulariodecontato').addEventListener('submit', function(event) {
+document.getElementById('Formulariodecontato').addEventListener('submit', function(event) {  //pegando no meu html o valor contido no meu formula rio com id = Formulariodecontato, ao clicar no botão que tem a ação 'submit' (submeter / enviar)
     event.preventDefault();
-    alert('Mensagem enviada com sucesso!');
-    // Aqui você vai adicionar código para enviar o formulário via wpp, depois
+
+    var nome = document.getElementById('nome').value; //pegando no meu html o valor escrito no campo com id = nome
+
+    var email = document.getElementById('email').value; //pegando no meu html o valor escrito no campo com id = email
+
+    var mensagem = document.getElementById('mensagem').value;  //pegando no meu html o valor escrito no campo com id = mensagem
+
+    var textoEnviar = `Meu nome é ${nome}, meu email é ${email}. ${mensagem}`; //montando a mensagem que vou enviar pro wpp
+
+    var linkWpp = `https://wa.me/5567993455044?text=${textoEnviar}`; //montando o link que direciona para o wpp com o texto digitado pelo usuário
+
+    console.log(nome) //exbindo  no console
+    console.log(email) //exbindo  no console
+    console.log(mensagem) //exbindo  no console
+    console.log(textoEnviar) //exbindo  no console
+
+    
+    alert('Mensagem enviada com sucesso!'); //exibindo pro usuário que a mensagem foi enviada
+
+    window.open(linkWpp, '_blank') //abrindo em outra janela o aplicativo / url do wpp
 });
